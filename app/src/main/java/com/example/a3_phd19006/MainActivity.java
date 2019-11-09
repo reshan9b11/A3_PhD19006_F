9174c78f31531c5b7dd86b5fbddca7813e7689f1
reshanriq <reshanofficial@gmail.com>
2019-11-09 20:02:50 +0530

    c3

--- app/src/main/java/com/example/a3_phd19006/MainActivity.java
+++ app/src/main/java/com/example/a3_phd19006/MainActivity.java
@@ -2,23 +2,39 @@ package com.example.a3_phd19006;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     static final String LOG_TAG = MainActivity.class.getSimpleName();
     private String filepath;
     private TextView acc;
+    private ListView wifilist;
+    private List<ScanResult> result;
+    private ArrayList<String> arrayList=new ArrayList<>();
+    private ArrayAdapter adapter;
+    private WifiManager wifiManager;
+    Button buttonScan;
 
    // private DBHelper mydb
 
@@ -40,6 +56,35 @@ public class MainActivity extends AppCompatActivity {
 //        Log.e(LOG_TAG, filepath);
 //        acc=(TextView)findViewById(R.id.acc_show);
 //        acc.setText("X= "+xxx+"Y= "+yyy+"z= "+zzz);
+        buttonScan = findViewById(R.id.wifi_scan);
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanWifi();
+            }
+        });
+
+
+        wifilist=findViewById(R.id.wifi_list);
+        wifiManager=(WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if(!wifiManager.isWifiEnabled()){
+            Toast.makeText(this ,"WIFI is disable, Enable it", Toast.LENGTH_SHORT).show();
+            wifiManager.setWifiEnabled(true);
+        }
+        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,arrayList);
+       // Log.d("g",arrayList.get(1));
+        wifilist.setAdapter(adapter);
+
+
+
+
+
+
+
+
+
+
+
     }
 
     public void onPressStartService(View v){
@@ -87,4 +132,34 @@ public class MainActivity extends AppCompatActivity {
         startActivity(x);
 
     }
+
+
+
+
+
+    public void scanWifi(){
+        arrayList.clear();
+        registerReceiver(wifiReceiver,new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        Log.d("dfsfsf","ScanningWifif");
+        wifiManager.startScan();
+    }
+
+
+    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            result = wifiManager.getScanResults();
+            unregisterReceiver(this);
+            Log.d("k","Broadcasr");
+
+            for (ScanResult scanResult : result) {
+                arrayList.add(scanResult.SSID + " - " + scanResult.capabilities);
+                Log.d("kx",scanResult.SSID);
+
+                adapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+
 }
